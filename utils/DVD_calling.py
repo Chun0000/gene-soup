908535d0025fadf345d864357f094b1c8b4086ad
Your Name <you@example.com>
2023-12-22 10:49:26 +0800

    Code correction for Clivar and Change Path

--- utils/DVD_calling.py
+++ utils/DVD_calling.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import utils.return_offical_symbol as offi
 # import return_offical_symbol as offi
-dvd = pd.read_csv('./database/DVD_final_w_disease.txt', sep='\t')
+dvd = pd.read_csv('/Volumes/ANTHONY/database/DVD_final_w_disease.txt', sep='\t')
+# dvd = pd.read_csv('./database/DVD_final_w_disease.txt', sep='\t')
 # dvd = pd.read_csv(
 #     '/Users/chun/Documents/GitHub/variant-search-engine/database/DVD_final_w_disease.txt', sep='\t')
 
